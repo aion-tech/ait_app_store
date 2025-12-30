6844b03810b1d493b90384065f2de4e7581b5418
Guidomadormo <68272608+Guidomadormo@users.noreply.github.com>
2025-12-30 12:36:20 +0100

    Update __manifest__.py

--- ait_attachment_utils/__manifest__.py
+++ ait_attachment_utils/__manifest__.py
@@ -4,9 +4,10 @@
     "description": "",
     "author": "Aion Tech Srl",
     "website": "https://aion-tech.it/",
+    "license": "LGPL-1",
     # https://github.com/odoo/odoo/blob/14.0/odoo/addons/base/data/ir_module_category_data.xml
     "category": "Uncategorized",
-    "version": "16.0.1.0.0",
+    "version": "16.0.1.0.1",
     "depends": [
         "base",
         # "web_notify",
