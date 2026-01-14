58798e53760cc0b0e4b8f155f95cb10c6bde2026
FrancescoGermani <francesco.germani@aion-tech.it>
2026-01-14 13:42:13 +0100

    fake commit

--- ait_attachment_utils/__manifest__.py
+++ ait_attachment_utils/__manifest__.py
@@ -7,7 +7,7 @@
     "license": "LGPL-3",
     # https://github.com/odoo/odoo/blob/14.0/odoo/addons/base/data/ir_module_category_data.xml
     "category": "Uncategorized",
-    "version": "16.0.1.0.2",
+    "version": "16.0.1.0.3",
     "depends": [
         "base",
         # "web_notify",
